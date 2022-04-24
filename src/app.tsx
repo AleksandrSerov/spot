bdb4d20ce1a8af7201e88e81cbf9d54c91104ab5
Aleksandr Serov <sashaserov2010@gmail.com>
2022-04-24 15:02:41 +0300

    fix(*): remove old stuff

--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import { Layer, Stage } from 'react-konva';
+import { Canvas } from '@react-three/fiber';
 
-import { Map } from './components/map';
-import { Cell } from './components/player';
+import { Box } from './components/box';
 
-import styles from './app.module.css';
-
-const generateCells = () => {
-	const cells = [];
-
-	for (let i = 0; i < 10; i++) {
-		for (let j = 0; j < 10; j++) {
-			const pos = [i, j];
-
-			cells.push(pos);
-		}
-	}
-
-	return cells;
-};
-
-const sells = generateCells();
-
-export const App: React.FC = () => (
-	<Stage className={ styles.component } width={ window.innerWidth } height={ window.innerHeight }>
-		<Layer>
-			<Map>
-				{/* <Grid width={ 500 } height={ 500 } visible={ false } /> */}
-				{sells.map(([x, y], index) => (
-					<Cell key={ `${x}_${y}` } startPos={ { x, y } } />
-				))}
-			</Map>
-		</Layer>
-	</Stage>
+export const App = () => (
+	<Canvas>
+		<ambientLight />
+		<pointLight position={ [10, 10, 10] } />
+		<Box position={ [-1.2, 0, 0] } />
+		<Box position={ [1.2, 0, 0] } />
+	</Canvas>
 );
--- src/utils/sum/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from '.';
-
-describe('test', () => {
-	it('should work', () => {
-		expect(sum(1, 2)).toEqual(3);
-	});
-});
