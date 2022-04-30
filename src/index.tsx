c357a0c673b8041b9b2aadb46190dbc72cacb36c
Aleksandr Serov <sashaserov2010@gmail.com>
2022-04-30 18:13:25 +0300

    feat(*): add stats

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Stats from 'stats.js';
 
 import { App } from './app';
 
 import './index.module.css';
+
+const stats = new Stats();
+
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
+
+function animate() {
+	stats.begin();
+
+	// monitored code goes here
+
+	stats.end();
+
+	requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
 
 const container = document.getElementById('app');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
